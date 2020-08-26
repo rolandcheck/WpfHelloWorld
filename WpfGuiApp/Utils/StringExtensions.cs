feda2786b69e36bf775510e50120ea58439066a7
rolandcheck <nafis5559@gmail.com>
2020-08-26 11:48:34 +0300

    removed some branching

--- WpfGuiApp/Utils/StringExtensions.cs
+++ WpfGuiApp/Utils/StringExtensions.cs
@@ -4,9 +4,6 @@
     {
         public static string TrimEnd(this string source, string value)
         {
-            if (!source.EndsWith(value))
-                return source;
-
             return source.Remove(source.LastIndexOf(value));
         }
     }
